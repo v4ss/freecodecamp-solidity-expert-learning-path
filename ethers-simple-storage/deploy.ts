71371dd65c7b835b9198006a73bae5c15225b580
v4ss <contact.v4ss@gmail.com>
2023-11-28 10:32:49 +0100

    replace js by ts

--- ethers-simple-storage/deploy.ts
+++ ethers-simple-storage/deploy.ts
@@ -1,18 +1,18 @@
-const ethers = require("ethers");
-const fs = require("fs-extra");
-require("dotenv").config();
+import {ethers} from "ethers"
+import * as fs from "fs-extra"
+import "dotenv/config"
 
 async function main() {
     // compile them in our code
     // http://127.0.0.1:7545
-    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
-    // const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-    const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf-8");
-    let wallet = new ethers.Wallet.fromEncryptedJsonSync(
-        encryptedJson,
-        process.env.PRIVATE_KEY_PASSWORD,
-    );
-    wallet = await wallet.connect(provider);
+    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL!);
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider);
+    // const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf-8");
+    // let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+    //     encryptedJson,
+    //     process.env.PRIVATE_KEY_PASSWORD!,
+    // );
+    // wallet = await wallet.connect(provider);
 
     const abi = fs.readFileSync(
         "./SimpleStorage_sol_SimpleStorage.abi",
@@ -26,6 +26,7 @@ async function main() {
     console.log("Deploying, please wait...");
     const contract = await contractFactory.deploy(); // STOP here! Wait for contract deployed
     await contract.deployTransaction.wait(1); // Wait the confirmation of the block is create
+    console.log(`Contract address: ${contract.address}`);
 
     // Get number
     const currentFavoriteNumber = await contract.retrieve();
