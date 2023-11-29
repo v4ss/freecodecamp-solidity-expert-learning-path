b3df03d7b6a575d4aed7d8dd08727daf9c630f5f
v4ss <contact.v4ss@gmail.com>
2023-11-29 14:31:12 +0100

    Add Hardhat tests and gas-reporter

--- hardhat-simple-storage/hardhat.config.js
+++ hardhat-simple-storage/hardhat.config.js
@@ -2,10 +2,12 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 //require("@nomiclabs/hardhat-etherscan");
 require("./tasks/block-number.js");
+require("hardhat-gas-reporter");
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -24,5 +26,13 @@ module.exports = {
     solidity: "0.8.19",
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-reporter.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        token: "ETH",
     },
 };
---
+++ hardhat-simple-storage/test/test-deploy.js
@@ -0,0 +1,31 @@
+const { ethers } = require("hardhat");
+const { expect, assert } = require("chai");
+
+describe("SimpleStorage", function () {
+    let simpleStorageFactory, simpleStorage;
+
+    beforeEach(async function () {
+        simpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+        simpleStorage = await simpleStorageFactory.deploy();
+    });
+
+    it("Should start with a favorite number of 0", async function () {
+        const currentValue = await simpleStorage.retrieve();
+        const expectedValue = "0";
+
+        // Two methods
+        // assert
+        // expect
+        assert.equal(currentValue.toString(), expectedValue);
+        //expect(currentValue.toString()).to.equal(expectedValue);
+    });
+
+    it("Should update the favorite number when we call store", async function () {
+        const expectedValue = "7";
+        const transactionResponse = await simpleStorage.store(expectedValue);
+        await transactionResponse.wait(1);
+        const updatedValue = await simpleStorage.retrieve();
+
+        assert.equal(updatedValue.toString(), expectedValue);
+    });
+});
