76801f2682d14e578510deeac9e7191e41f2295d
v4ss <contact.v4ss@gmail.com>
2023-11-29 11:43:48 +0100

    Add Verify, Tasks, localhost network

--- hardhat-simple-storage/hardhat.config.js
+++ hardhat-simple-storage/hardhat.config.js
@@ -1,19 +1,28 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
-require("@nomiclabs/hardhat-etherscan");
+//require("@nomiclabs/hardhat-etherscan");
+require("./tasks/block-number.js");
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  defaultNetwork: "hardhat",
-  networks: {
-    sepolia: {
-      url: SEPOLIA_RPC_URL,
-      accounts: [PRIVATE_KEY],
-      chainId: 11155111,
+    defaultNetwork: "hardhat",
+    networks: {
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 11155111,
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            chainId: 31337,
+        },
     },
-  },
-  solidity: "0.8.19",
+    solidity: "0.8.19",
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
 };
--- hardhat-simple-storage/scripts/deploy.js
+++ hardhat-simple-storage/scripts/deploy.js
@@ -1,17 +1,45 @@
 // imports
-const { ethers } = require("hardhat");
+const { ethers, run, network } = require("hardhat");
 
 // async main
 async function main() {
-    const SimpleStorageFactory = await ethers.getContractFactory(
-        "SimpleStorage"
-    );
+    const SimpleStorageFactory =
+        await ethers.getContractFactory("SimpleStorage");
     console.log("Deploying contract...");
     const simpleStorage = await SimpleStorageFactory.deploy();
+    await simpleStorage.waitForDeployment();
     console.log(`Deployed contract to: ${simpleStorage.target}`);
+    // What happens when we deploy to our hardhat network?
+
+    if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+        console.log("Waiting for the txes");
+        await simpleStorage.deploymentTransaction().wait(6);
+        await verify(simpleStorage.target, []);
+    }
+
+    const currentValue = await simpleStorage.retrieve();
+    console.log(`Current Value is: ${currentValue}`);
+    const transactionResponse = await simpleStorage.store("7");
+    await transactionResponse.wait(1);
+    const updatedValue = await simpleStorage.retrieve();
+    console.log(`Updated Value is: ${updatedValue}`);
 }
 
-async function verify(contractAddress, args) {}
+async function verify(contractAddress, args) {
+    console.log("Verifying contract ...");
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        });
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already Verified!");
+        } else {
+            console.log(e);
+        }
+    }
+}
 
 // main
 main()
