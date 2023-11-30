31351a5ffbc3ab2e881774839a2042ecf7ae0ae5
v4ss <contact.v4ss@gmail.com>
2023-11-30 15:02:03 +0100

    Add verify

--- hardhat-fund-me/deploy/01-deploy-fund-me.js
+++ hardhat-fund-me/deploy/01-deploy-fund-me.js
@@ -3,6 +3,7 @@ const {
     networkConfig,
     developmentChains,
 } = require("../helper-hardhat-config");
+const { verify } = require("../utils/verify");
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments;
@@ -18,11 +19,22 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
         ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
     }
 
+    const args = [ethUsdPriceFeedAddress];
+
     const fundMe = await deploy("FundMe", {
         from: deployer,
-        args: [ethUsdPriceFeedAddress],
+        args: args,
         log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
     });
+
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(fundMe.address, args);
+    }
+    log("------------------------------------");
 };
 
 module.exports.tags = ["all", "fundme"];
--- hardhat-fund-me/hardhat.config.js
+++ hardhat-fund-me/hardhat.config.js
@@ -10,8 +10,9 @@ module.exports = {
     networks: {
         sepolia: {
             url: process.env.SEPOLIA_RPC_URL || "",
-            accounts: [],
+            accounts: [process.env.PRIVATE_KEY],
             chainId: 11155111, //To know the chainId, go to chainlist.org
+            blockConfirmations: 6,
         },
         localhost: {
             url: "http://127.0.0.1:8545/",
@@ -25,7 +26,7 @@ module.exports = {
         apiKey: process.env.ETHERSCAN_API_KEY || "",
     },
     gasReporter: {
-        enabled: true,
+        enabled: false,
         outputFile: "gas-reporter.txt",
         noColors: true,
         currency: "USD",
@@ -35,7 +36,6 @@ module.exports = {
     namedAccounts: {
         deployer: {
             default: 0,
-            11155111: 1,
         },
         users: {
             default: 1,
